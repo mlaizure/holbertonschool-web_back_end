48a445b9e0d42cabf82c95eabd65260a012b5e92
mlaizure <maddi.laizure@gmail.com>
2021-10-18 18:56:41 +0000

    Changing order

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -3,6 +3,6 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """returns sum of mxd_lst"""
     return sum(mxd_lst)
