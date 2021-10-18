2cf066b0d18800247f4e488b55acd57c3203a1e4
mlaizure <maddi.laizure@gmail.com>
2021-10-18 18:46:06 +0000

    Task 5 and main files

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""contains sum_list function"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """returns sum of input_list"""
+    return sum(input_list)
