9b1202aa60717d254e99fec2b700dcf9a4d55801
mlaizure <maddi.laizure@gmail.com>
2021-10-20 17:04:53 +0000

    Correcting type annotation

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[float]:
+async def async_generator() -> Generator[float, None, None]:
     """asynchronous generator for 10 random numbers that waits 1 sec between"""
     for i in range(10):
         yield random.uniform(0, 10)
