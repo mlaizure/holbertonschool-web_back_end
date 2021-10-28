88c0e0d59ca80f9e81fff15b0717b06a8172af90
mlaizure <maddi.laizure@gmail.com>
2021-10-28 23:13:18 +0000

    Task 1

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """contains filter_datum function"""
 from typing import List
 import re
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -11,3 +12,21 @@ def filter_datum(fields: List[str], redaction: str,
         regex = f"{field}([^{separator}]*{separator})"
         message = re.sub(regex, f"{field}={redaction}{separator}", message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """filters values in log records"""
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
