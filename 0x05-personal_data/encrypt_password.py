de1193a989c4c48944504b591dbbaa3424062347
mlaizure <maddi.laizure@gmail.com>
2021-10-29 19:00:42 +0000

    Task 6

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -6,3 +6,8 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """returns salted, hashed password as byte string"""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """validates the provided password matches the hashed password"""
+    return bcrypt.checkpw(password.encode(), hashed_password)
