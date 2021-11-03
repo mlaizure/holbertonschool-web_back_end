0b6fda2ad86157542b5e7ac3c942b956d604bd8c
mlaizure <maddi.laizure@gmail.com>
2021-11-03 00:22:55 +0000

    Task 6

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -15,7 +15,10 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 auth = os.getenv('AUTH_TYPE', None)
-if auth:
+if auth == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+elif auth:
     from api.v1.auth.auth import Auth
     auth = Auth()
 
---
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Basic authentication module for API
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """basic authentication handling"""
+    pass
