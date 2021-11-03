8baffc6f7fc4d2b309a73116ae57dbf952f4efb5
mlaizure <maddi.laizure@gmail.com>
2021-11-03 19:03:25 +0000

    Task 9

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -27,3 +27,13 @@ class BasicAuth(Auth):
         except base64.binascii.Error:
             return None
         return decoded_header.decode('utf-8')
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """returns user email and password from Base64 decoded value"""
+        if decoded_base64_authorization_header is None or \
+           type(decoded_base64_authorization_header) != str:
+            return (None, None)
+        if ':' not in decoded_base64_authorization_header:
+            return (None, None)
+        return tuple(decoded_base64_authorization_header.split(':'))
