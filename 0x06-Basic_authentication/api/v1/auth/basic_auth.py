cdabc8d5d02ba05edd08a085a95505fd44761272
mlaizure <maddi.laizure@gmail.com>
2021-11-03 18:44:26 +0000

    Task 8

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 Basic authentication module for API
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -14,3 +15,15 @@ class BasicAuth(Auth):
            or authorization_header[0:6] != 'Basic ':
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """returns decoded value of Base64 str base64_authorization_header"""
+        if base64_authorization_header is None or \
+           type(base64_authorization_header) != str:
+            return None
+        try:
+            decoded_header = base64.b64decode(base64_authorization_header)
+        except base64.binascii.Error:
+            return None
+        return decoded_header.decode('utf-8')
