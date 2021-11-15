e4f90010e05e6de8459fb485f7d2da96e17c12a9
mlaizure <maddi.laizure@gmail.com>
2021-11-15 22:10:49 +0000

    Task 12

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -6,6 +6,7 @@ from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
 from uuid import uuid4
+from typing import Union
 
 
 def _generate_uuid() -> str:
@@ -51,3 +52,12 @@ class Auth:
             return None
         self._db.update_user(user.id, session_id=_generate_uuid())
         return user.session_id
+
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
+        """returns user corresponding to session id or None if no user"""
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
