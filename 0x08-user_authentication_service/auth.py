b447a71488e2d16fedef35f682ee7a6a16f82dfe
mlaizure <maddi.laizure@gmail.com>
2021-11-15 23:55:16 +0000

    Task 16

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -66,3 +66,14 @@ class Auth:
     def destroy_session(self, user_id: int) -> None:
         """updates corresponding user's session id to None"""
         self._db.update_user(user_id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        """find corresponding user and update reset token"""
+        if email is None:
+            return None
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        self._db.update_user(user.id, reset_token=_generate_uuid())
+        return user.reset_token
