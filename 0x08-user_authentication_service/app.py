9f2b563ff6641497b70eca5bc817bb4d92121f78
mlaizure <maddi.laizure@gmail.com>
2021-11-16 00:02:15 +0000

    Task 17

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -68,5 +68,17 @@ def profile():
     return jsonify(email=user.email), 200
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token():
+    """generate a token and respond with email and reset token"""
+    email = request.form.get('email')
+    try:
+        AUTH._db.find_user_by(email=email)
+    except NoResultFound:
+        abort(403)
+    reset_token = AUTH.get_reset_password_token(email)
+    return jsonify(email=email, reset_token=reset_token)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
