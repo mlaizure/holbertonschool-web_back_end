0d090b9141d2949817c186ba48af6063b511fa2f
mlaizure <maddi.laizure@gmail.com>
2021-11-22 02:32:08 +0000

    Task 5

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """module for testing client.py"""
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -19,3 +19,12 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(test.org, {'k': 'v'})
         url = 'https://api.github.com/orgs/{}'.format(org_name)
         mock_gj.assert_called_once_with(url)
+
+    def test_public_repos_url(self):
+        """tests that public_repos_url returns correct payload"""
+        with patch(
+                'client.GithubOrgClient.org',
+                new_callable=PropertyMock(
+                    return_value={'repos_url': 'test_url'})):
+            test = GithubOrgClient('test_org')
+            self.assertEqual(test._public_repos_url, 'test_url')
