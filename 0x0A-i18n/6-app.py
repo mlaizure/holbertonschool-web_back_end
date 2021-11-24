17115eac59cce234a15d81dc8caf0609ab0a6316
mlaizure <maddi.laizure@gmail.com>
2021-11-24 21:54:56 +0000

    Fixing syntax

--- 0x0A-i18n/6-app.py
+++ 0x0A-i18n/6-app.py
@@ -46,7 +46,7 @@ def get_locale():
         elif not locale:
             locale = request.accept_languages.best_match(
                 app.config['LANGUAGES']) or \
-                BABEL_DEFAULT_LOCALE
+                Config.BABEL_DEFAULT_LOCALE
 
     return locale
 
