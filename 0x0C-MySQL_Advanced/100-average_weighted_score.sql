cf5389c344d5e73455eaf57f0b2c113b83bd9d88
mlaizure <maddi.laizure@gmail.com>
2021-12-03 01:58:19 +0000

    Task 12 and narrowing join

--- 0x0C-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x0C-MySQL_Advanced/100-average_weighted_score.sql
@@ -4,7 +4,7 @@ DELIMITER //
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser(user_id INT)
 BEGIN
 	SET @wtd_score := (SELECT SUM(score * weight) / SUM(weight)
-	    FROM corrections LEFT JOIN projects
+	    FROM corrections INNER JOIN projects
 	    ON corrections.project_id = projects.id
 	    WHERE user_id = corrections.user_id);
 	UPDATE users SET average_score = @wtd_score WHERE id = user_id;
