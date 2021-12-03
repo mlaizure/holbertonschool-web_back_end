b3232323bed804cc8489a11287e0547f37bb2134
mlaizure <maddi.laizure@gmail.com>
2021-12-03 02:04:19 +0000

    Deleting unnecessary join

--- 0x0C-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x0C-MySQL_Advanced/101-average_weighted_score.sql
@@ -3,7 +3,7 @@
 DELIMITER //
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-	UPDATE users INNER JOIN corrections ON corrections.user_id = users.id
+	UPDATE users
 	    SET average_score = (SELECT SUM(score * weight) / SUM(weight)
 	    FROM corrections INNER JOIN projects
 	    ON corrections.project_id = projects.id
