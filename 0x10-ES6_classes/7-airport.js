c89a759826ce5efb84e8438d8df4a53b6168f921
mlaizure <maddi.laizure@gmail.com>
2021-12-16 23:24:20 +0000

    eslint fixes

--- 0x10-ES6_classes/7-airport.js
+++ 0x10-ES6_classes/7-airport.js
@@ -1,10 +1,10 @@
 export default class Airport {
-  constructor(name, code){
+  constructor(name, code) {
     this._name = name;
     this._code = code;
   }
 
   toString() {
-    return `[object ${this._code}]`
+    return `[object ${this._code}]`;
   }
 }
